03f8c03babc2b891174b2241299c9fd098bd5a91
GhostPacket <harsh.physicsquantum@example.com>
2026-09-25 00:41:07 +0530

    Added Socket and port validation

--- port_scanner.py
+++ port_scanner.py
@@ -1,6 +1,25 @@
+import socket 
+
 print("Port Scanner Project V-1.0")
 
 ip = input("Enter the IP: ")
-port = int(input("Enter the port number: "))
+try:
+    port = (input("Enter the port number: "))
+    port = int(port)
+except:
+    print("Invalid port")
+    exit()
 
 print("The IP to be scanned is: ", ip, " and the port is: ", port )
+
+if (port < 1 or port > 65535):
+    print("Invalid Port number")
+    exit()
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    try:
+        s.settimeout(1)
+        s.connect((ip,port))
+        print("Port is open.")
+    except socket.error:
+        print("Port is closed.")
