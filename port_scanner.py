bd3da371888659248e47d1f286c825f100b66af9
GhostPacket <harsh.physicsquantum@example.com>
2026-09-25 00:00:19 +0530

    Initial commit

---
+++ port_scanner.py
@@ -0,0 +1,6 @@
+print("Port Scanner Project V-1.0")
+
+ip = input("Enter the IP: ")
+port = int(input("Enter the port number: "))
+
+print("The IP to be scanned is: ", ip, " and the port is: ", port )
