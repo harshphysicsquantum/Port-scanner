4112b4449330b5d920f04e2dfd8c21c1dd6a3b0c
GhostPacket <harsh.physicsquantum@example.com>
2026-09-26 00:53:25 +0530

    Threads added

--- port_scanner.py
+++ port_scanner.py
@@ -1,25 +1,74 @@
 import socket 
+import time
+import threading
+
 
 print("Port Scanner Project V-1.0")
 
 ip = input("Enter the IP: ")
 try:
-    port = (input("Enter the port number: "))
-    port = int(port)
+    start_port = (input("Enter the starting port number: "))
+    start_port = int(start_port)
+    last_port = input("Enter the last port number: ")
+    last_port = int(last_port)
 except:
     print("Invalid port")
     exit()
 
-print("The IP to be scanned is: ", ip, " and the port is: ", port )
+print("The IP to be scanned is: ", ip, " and the ports to be scanned are: ", start_port, last_port )
 
-if (port < 1 or port > 65535):
-    print("Invalid Port number")
+if (start_port < 1 or start_port > 65535):
+    print("Invalid start Port number")
     exit()
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    try:
-        s.settimeout(1)
-        s.connect((ip,port))
-        print("Port is open.")
-    except socket.error:
-        print("Port is closed.")
+if (last_port < 1 or last_port > 65535):
+    print("Invalid last Port number ")
+    exit()
+
+if (start_port > last_port):
+    print("Invalid range")
+    exit()
+
+open_ports = []
+def scan_port(port):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        try:
+            s.settimeout(1)
+            s.connect((ip,port))
+            try:
+                service_name = socket.getservbyport(port, "tcp")
+            except OSError:
+                service_name = "Unknown"
+            port_info = (port, service_name)
+            open_ports.append(port_info)
+        except socket.error:
+            # print("Port", port,  "is closed.")
+            pass
+
+
+threads=[]
+
+for port in range(start_port, last_port + 1):
+    t = threading.Thread(target = scan_port, args = (port,))
+    threads.append(t)
+
+start_time = time.perf_counter()
+
+for t in threads:
+    t.start()
+
+for t in threads:
+    t.join()
+
+end_time = time.perf_counter()
+elapsed_time = end_time - start_time
+
+
+if open_ports:
+    print("\nOpen Ports:")
+    for port, service in open_ports:
+        print(f"  {port}/tcp  →  {service}")
+else:
+    print("\nNo open ports found.")
+
+print(f"Scan completed in: {elapsed_time:.2f} seconds")
